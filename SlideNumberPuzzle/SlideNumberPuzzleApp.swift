f6f089e1d1faff96a918b24e2ffe5bdfe0a477bc
DOmENos <domhaha9@gmail.com>
2023-11-18 18:48:33 +0700

    Initial Commit

---
+++ SlideNumberPuzzle/SlideNumberPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideNumberPuzzleApp.swift
+//  SlideNumberPuzzle
+//
+//  Created by Setthanan Thitathanapat on 18/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SlideNumberPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
